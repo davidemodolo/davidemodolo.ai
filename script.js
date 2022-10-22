7cc377a6b2510c4937ac7f4dc071c6b7a5c3bbc7
Davide Modolo <davide.modolo98@gmail.com>
2022-10-22 11:29:48 +0200

    Fixing courses mess

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
 import projects_json from './projects.json' assert { type: 'json' };
-
-// log the projects_json length to the console
 const projects = projects_json.length;
 
-// same for the courses_json
 import courses_json from './courses.json' assert { type: 'json' };
-const exams = courses_json.length;
-
-
-function sendEmail() {
-    const email = 'dott.modolo@gmail.com';
-
-    if (typeof screen.orientation !== 'undefined') {
-        //alert(`My email is: ${email}`)
-
-    } else {
-        const subject = 'Found your site';
-        const emailBody = 'Minimum RAL = 40k';
-        document.location = "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
-    }
-
-}
+const courses = courses_json.length;
 
 
 const div_projects = document.querySelector('.project-list');
 for (let i = 0; i < projects; i++) {
     const prj = document.createElement('div');
-    const prj_name = projects_json[i]['project'];
     prj.classList.add('project');
-    //prj.style.backgroundColor = '#00FF00'
-    const htmlvalue = `${prj_name}<br>Descrizione`
-    prj.innerHTML = htmlvalue;
+
+    const prj_name = projects_json[i]['project'];
+    prj.innerHTML = prj_name;
 
     // add project description from json to another internal div
     const prj_desc = document.createElement('div');
@@ -63,8 +44,6 @@ for (let i = 0; i < projects; i++) {
         prj_pics.appendChild(pic);
     }
     prj.appendChild(prj_pics);
-    
-
 
 
     prj.addEventListener('click', function() { console.log(prj_name) });
@@ -76,44 +55,27 @@ for (let i = 0; i < projects; i++) {
 
 const div_courses = document.querySelector('.courses-list');
 for (let i = 0; i < courses; i++) {
-    var course_name = courses_json[i]['course'];
-
+    const course_name = courses_json[i]['course'];
     const course = document.createElement('div');
-    course_name = courses_json[i]['course'];
-    const progress = courses_json[i]['progression'];
-    // show the progress bar
-    const progress_bar = document.createElement('div');
-    // what is classlist?
-    progress_bar.classList.add('progress-bar');
-    progress_bar.style.width = `${progress}%`;
-    progress_bar.innerHTML = `${progress}%`;
-    // add a button to start the exam
-    const start_exam = document.createElement('button');
-    start_exam.classList.add('start-exam');
-    start_exam.innerHTML = 'Start Exam';
-    start_exam.addEventListener('click', function() { console.log(course_name) });
-    // add a button to see the exam
-    const see_exam = document.createElement('button');
-    see_exam.classList.add('see-exam');
-    see_exam.innerHTML = 'See Exam';
-    see_exam.addEventListener('click', function() { console.log(course_name) });
-    // add a button to see the exam
-    const see_results = document.createElement('button');
-    see_results.classList.add('see-results');
-    see_results.innerHTML = 'See Results';
-    see_results.addEventListener('click', function() { console.log(course_name) });
-    // append all the elements to the course div
-    course.appendChild(progress_bar);
-    course.appendChild(start_exam);
-    course.appendChild(see_exam);
-    course.appendChild(see_results);
-    // append the course div to the exam-list div
+    course.classList.add('course');
+    course.innerHTML = course_name;
+    // add progress bar from json
+    const course_progress = document.createElement('div');
+    course_progress.classList.add('course-progress');
+
+    const progress_value = courses_json[i]['progress'];
+    course_progress.style.width = progress_value+'%';
+    course_progress.innerHTML = progress_value+'%';
+    // set color of progress bar based on progress
+    if (progress_value < 35) {
+        course_progress.style.backgroundColor = '#53B2E8';
+    } else if (progress_value < 75) {
+        course_progress.style.backgroundColor = '#53E8BB';
+    } else {
+        course_progress.style.backgroundColor = '#53E859';
+    }
+    course.appendChild(course_progress);
+
     div_courses.appendChild(course);
 
 }
-
-    /*const exm_name = courses_json[i]['course'];
-    exm.classList.add('exam');
-    exm.innerHTML = `${exm_name}<br>Descrizione`;
-    exm.addEventListener('click', function() { console.log(exm_name) });
-    div_exam.appendChild(exm);*/
